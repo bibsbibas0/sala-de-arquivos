6d6b72ccf988ff6f7aa603d663a0c315097ae190
bibsbibas0 <danielmmedeiros24@gmail.com>
2025-08-08 14:38:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function verificarSenha() {
     let senha = document.getElementById("senha").value;
     let erro = document.getElementById("erro");
 
-    if(senha === "1891") { // senha secreta
+    if(senha === "0407") { // senha secreta
         document.getElementById("porta").style.display = "none";
         document.getElementById("sala").style.display = "block";
     } else {
